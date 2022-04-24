3aad5000e69932b975e926b312cf4149dbf497fc
Pyro <85371234+PyroTheLemon@users.noreply.github.com>
2022-04-25 00:40:35 +0300

    Update anglecalculator.py
    Fixed some bugs in printing the calculation's

--- anglecalculator.py
+++ anglecalculator.py
@@ -3,7 +3,7 @@ import time
 def internal_angle():
     return (angle_number-2)*180
 def single_angle():
-    return internal_angle()/angle_number
+    return internal_angle() / angle_number
 def external_angle():
     return 180-single_angle()
 #CODED BY Pyro.I
@@ -17,21 +17,21 @@ while True:
     choice = input("Enter choice(1/2/3/4): ")
     if choice in ('1', '2', '3', '4'):
             angle_number = float(input("Give the number of corner's: "))
-            
+
             if choice == '1':
-                print("(", angle_number, "- 2 ) * 180 = ", internal_angle())
+                print(angle_number, "- 2.0) 180 = ", internal_angle())
             elif choice == '2':
                 print(internal_angle(), "/", angle_number, "=", single_angle())
             elif choice == '3':
-                print("180 -", internal_angle(), "=", external_angle())
+                print("180 -", single_angle(), "=", external_angle())
             elif choice == '4':
-                print("(", angle_number, "- 2 ) * 180 = ", internal_angle(), "| <-- Internal Angle")
+                print("(", angle_number, "- 2 ) * 180=", internal_angle(), "| <-- Internal Angle")
                 print(internal_angle(), "/", angle_number, "=", single_angle(), "| <-- Single Angle")
-                print("180 -", internal_angle(), "=", external_angle(), "| <-- External Angle")
+                print("180 -", single_angle(), "=", external_angle(), "| <-- External Angle")
             another_calculation = input("Wanna make another calculation?(Y/n): ")
             if another_calculation == "n":
-                print("Bye")
-                time.sleep(2.5)
+                print("Bye.")
+                time.sleep(1.5)
                 exit()
     else:
-            print("Please give valid input! --->(Y/n)")
+            print("Please give vaild input! --->(Y/n)") 
